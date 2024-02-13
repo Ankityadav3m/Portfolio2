6770569d1ffc8e65d5e99c03e0687d5da159d243
Ankit Yadav <ankityadav3m@gmail.com>
2024-02-13 22:37:11 +0530

    Updated portfolio

---
+++ script.js
@@ -0,0 +1,31 @@
+// Navbar Section
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
+
+// Home Section
+
+var typed = new Typed('#element', {
+  strings: ['Web Developer', 'Frontend Developer', 'RPA Developer', 'Python Developer', 'Web Designer'],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
+
+
+
+
+
+
+
+
+
+
+  
+ 
